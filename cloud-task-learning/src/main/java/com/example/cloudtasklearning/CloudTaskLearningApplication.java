2e6dc77210b86f11408a9bfab498643b99b339da
邱张华 <qiuzh@mail.taiji.com.cn>
2018-02-05 09:22:53 +0800

    Add ExitCodeExceptionMapper

--- cloud-task-learning/src/main/java/com/example/cloudtasklearning/CloudTaskLearningApplication.java
+++ cloud-task-learning/src/main/java/com/example/cloudtasklearning/CloudTaskLearningApplication.java
@@ -1,11 +1,15 @@
 package com.example.cloudtasklearning;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeExceptionMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,12 +19,24 @@ public class CloudTaskLearningApplication {
 
   @Bean
   public CommandLineRunner commandLineRunner() {
-    return strings ->
-        System.out.println("Executed at :" +
-            new SimpleDateFormat().format(new Date()));
+    return strings -> {
+      System.out.println("Executed at :" +
+          new SimpleDateFormat().format(new Date()));
+      Files.lines(Paths.get("/qqq/mmm")).count();
+    };
   }
 
   public static void main(String[] args) {
     SpringApplication.run(CloudTaskLearningApplication.class, args);
+  }
+
+  @Bean
+  public ExitCodeExceptionMapper exitCodeExceptionMapper() {
+    return (Throwable exception) -> {
+      Throwable t = exception.getCause();
+      if (t instanceof IOException) return 3;
+      return -1;
+    };
+
   }
 }
